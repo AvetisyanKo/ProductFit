ac6a299af200352143a3efbadc1420617d0d1f72
Koryun Avetisyan <ko.Avetisyan23@gamil.com>
2022-02-20 19:32:34 +0400

    add user select none

--- js/sectionScript.js
+++ js/sectionScript.js
@@ -1,49 +1,50 @@
-const sectionButton = document.getElementById("sectionButtonID");
-const backsectionButton = document.getElementById("backSectionButtonID");
-const buttonLink = sectionButton.children;
+document.addEventListener("DOMContentLoaded", () => {
+    const sectionButton = document.getElementById("sectionButtonID");
+    const backsectionButton = document.getElementById("backSectionButtonID");
+    const buttonLink = sectionButton.children;
 
-const sectionArrowPrev = document.querySelector("#sectionArrowPrev");
-const sectionArrowNext = document.querySelector("#sectionArrowNext");
+    const sectionArrowPrev = document.querySelector("#sectionArrowPrev");
+    const sectionArrowNext = document.querySelector("#sectionArrowNext");
 
-let count = 2;
-let windowWidth = window.innerWidth;
+    let count = 2;
+    let windowWidth = window.innerWidth;
 
-let element1 = buttonLink[1].cloneNode(true);
-let element2 = buttonLink[2].cloneNode(true);
+    let element1 = buttonLink[1].cloneNode(true);
+    let element2 = buttonLink[2].cloneNode(true);
 
-backsectionButton.style.paddingRight = "150px"
+    backsectionButton.style.paddingRight = "150px"
 
-backsectionButton.appendChild(element1);
-backsectionButton.appendChild(element2);
-
-
-sectionArrowNext.addEventListener("click", () => {
-    windowWidth = window.innerWidth;
-
-    if (windowWidth >= 740 && windowWidth <= 970) {
-        if (count > 4) return;
-
-        count++;
-        element1 = buttonLink[count - 1].cloneNode(true);
-        element2 = buttonLink[count].cloneNode(true);
-        backsectionButton.innerHTML = '';
-        backsectionButton.appendChild(element1);
-        backsectionButton.appendChild(element2);
-    }
-});
+    backsectionButton.appendChild(element1);
+    backsectionButton.appendChild(element2);
 
 
-sectionArrowPrev.addEventListener("click", () => {
-    windowWidth = window.innerWidth;
+    sectionArrowNext.addEventListener("click", (event) => {
+        windowWidth = window.innerWidth;   
+        if (windowWidth >= 740 && windowWidth <= 970) {
+            if (count > 4) return;
+            
+            count++;
+            element1 = buttonLink[count - 1].cloneNode(true);
+            element2 = buttonLink[count].cloneNode(true);
+            backsectionButton.innerHTML = '';
+            backsectionButton.appendChild(element1);
+            backsectionButton.appendChild(element2);
+        }
+    });
 
-    if (windowWidth >= 740 && windowWidth <= 970) {
-        if (count === 2) return;
 
-        count--;
-        element1 = buttonLink[count - 1].cloneNode(true);
-        element2 = buttonLink[count].cloneNode(true);
-        backsectionButton.innerHTML = '';
-        backsectionButton.appendChild(element1);
-        backsectionButton.appendChild(element2);
-    }
+    sectionArrowPrev.addEventListener("click", () => {
+        windowWidth = window.innerWidth;
+
+        if (windowWidth >= 740 && windowWidth <= 970) {
+            if (count === 2) return;
+
+            count--;
+            element1 = buttonLink[count - 1].cloneNode(true);
+            element2 = buttonLink[count].cloneNode(true);
+            backsectionButton.innerHTML = '';
+            backsectionButton.appendChild(element1);
+            backsectionButton.appendChild(element2);
+        }
+    });
 });
